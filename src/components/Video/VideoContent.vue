1c59535d8fb99c9b07e93e138802cc80d1bccf05
EdmundMC <1743220021@qq.com>
2022-07-18 09:30:55 +0800

    Add Video component
    添加视频页面，还未实现导航栏跳转与增加视频功能

---
+++ src/components/Video/VideoContent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="VideoContent">
+    <div class="VMenuWarp item">
+      <VideoNavMenu class="VMenu"></VideoNavMenu>
+    </div>
+    <div class="VideoWarp item">
+      <Video class="Video"></Video>      
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoNavMenu from "./VideoNavMenu.vue";
+import Video from "./Video.vue";
+
+export default {
+    components: {
+      VideoNavMenu,
+      Video
+    }
+}
+</script>
+  
+<style>
+#VideoContent{
+  display: flex;
+  margin-top: 100px;
+}
+.item{
+  justify-content: flex-start;
+  box-sizing: border-box;
+  border: 1px solid rgba(221, 7, 7, 0.37);
+  margin:5px;
+  background-color: rgba(141, 138, 138, 0.063);
+}
+.VMenuWarp{
+  margin: 5px;
+}
+.VMenu{
+  min-width: 205px;
+  margin: 10px;
+}
+.VideoWarp{
+  margin-left: 200px;
+}
+.Video{
+  margin-left:100px;
+  margin-top: 50px;
+  margin-bottom:100px;
+  margin-right: 100px;
+}
+</style>
---
+++ src/components/Video/VideoNavMenu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-radio-group 
+      v-model="isCollapse"
+      text-color="#FFFFFF"
+      style="margin-bottom: 20px;"
+      fill="#FF7356"
+      >
+      <el-radio-button :label="false" >展开</el-radio-button>
+      <el-radio-button :label="true" >收起</el-radio-button>
+    </el-radio-group>
+    <el-menu 
+      default-active="1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+      active-text-color="#FF7356"
+      background-color="#FFFFFF"
+      >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">视频列表</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">视频1</el-menu-item>
+          <el-menu-item index="1-2">视频2</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isCollapse: false
+      };
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-radio-group .el-radio-button .el-radio-button__inner:hover{
+    color:black;
+  }
+</style>
