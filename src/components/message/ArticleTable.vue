d06d0faee4ccac66a03b798af23870ff0a1aed7f
FYQ <fanyq1017@qq.com>
2022-07-19 18:47:06 +0800

    fix articletable

--- src/components/message/ArticleTable.vue
+++ src/components/message/ArticleTable.vue
@@ -1,215 +1,201 @@
 <template>
-  <div>
-    <el-header class="eheader">
-      <div style="display: flex; justify-content: flex-start; height: ">
-        <el-col span="">
-          <el-input
-            placeholder="通过活动名搜索"
-            prefix-icon="el-icon-search"
-            v-model="keywords"
-            style="width: 400px; height: 80px"
-          >
-          </el-input>
-        </el-col>
-        <el-col>
-          <el-button
-            type="primary"
-            icon="el-icon-search"
-            style="margin-top: 0; padding-top: 0; height: 70px"
-            @click="searchClick"
-            >搜索
-          </el-button>
-        </el-col>
-        <el-col offset="10" span="5">
-          <el-button
-            type="primary"
-            @click="addUser"
-            style="margin-top: 0; padding-top: 0; height: 70px"
-            >+新增</el-button
-          >
-        </el-col>
+<div class="row">
+  <div class="article-container">
+    <h4 class="container-title">信息动态</h4>
+    <ul class="article-list">
+      <li v-for="article in articles.slice(0, 6)" class="article-info">
+        <div class="title" @click="handleClick(article.id)">{{ article.title }}</div>
+        <div class="date">{{ article.publishDate  | formatDateTime}}</div>
+      </li>
+    </ul>
+    <h4 class="container-title">专题活动</h4>
+    <ul class="article-list">
+      <li v-for="article in articles.slice(6, 11)" class="article-info">
+        <div class="title">{{ article.title }}</div>
+        <div class="date">{{ article.publishDate  | formatDateTime}}</div>
+      </li>
+    </ul>
+    <h4 class="container-title">公益广告</h4>
+    <ul class="article-list">
+      <li v-for="article in articles.slice(11, 16)" class="article-info">
+        <div class="title">{{ article.title }}</div>
+        <div class="date">{{ article.publishDate  | formatDateTime}}</div>
+      </li>
+    </ul>
+
+  </div>
+
+  <div class="col2">
+    <div class="hotpoint-container">
+      <h4 class="container-title">热点信息</h4>
+      <ul class="article-list">
+        <li v-for="article in articles.slice(0, 3)" class="article-info">
+          <div class="title">{{ article.title }}</div>
+          <div class="date">{{ article.publishDate | formatDateTime }}</div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="hotimg-container">
+      <h4 class="container-title">热点图片</h4>
+
+      <div class="hot-img">
+        <img src="https://chinavolunteer.mca.gov.cn/NVSI/LEAP/Download/default/2022/5/27/eb6b953b6c144ba5bf7d91b89ac9cec4.jpg"/>
+        <h6>疫情防控志愿服务</h6> 
       </div>
-    </el-header>
-    <el-table
-      ref="multipleTable"
-      :data="articles"
-      tooltip-effect="dark"
-      style="width: 100%; overflow-x: hidden; overflow-y: hidden; height:800px;"
-      max-height="600"
-      @selection-change="handleSelectionChange"
-      v-loading="loading"
-    >
-      <el-table-column type="selection" width="35" align="left">
-      </el-table-column>
-      <el-table-column label="标题" width="400" align="left">
-        <template slot-scope="scope"
-          ><span
-            style="color: #409eff; cursor: pointer"
-            @click="itemClick(scope.row)"
-            >{{ scope.row.title }}</span
-          >
-        </template>
-      </el-table-column>
-      <el-table-column label="最近编辑时间" width="140" align="left">
-        <template slot-scope="scope">{{
-          scope.row.editTime | formatDateTime
-        }}</template>
-      </el-table-column>
-      <el-table-column prop="nickname" label="作者" width="120" align="left">
-      </el-table-column>
-      <el-table-column
-        prop="cateName"
-        label="所属分类"
-        width="120"
-        align="left"
-      >
-      </el-table-column>
-      <el-table-column label="操作" align="left">
-        <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-            >编辑
-          </el-button>
-          <el-button
-            size="mini"
-            @click="handleRestore(scope.$index, scope.row)"
-            v-if="showRestore"
-            >还原
-          </el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)"
-            >删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="blog_table_footer">
-      <el-button
-        type="danger"
-        style="margin: 0px; height: 40px"
-        v-show="this.articles.length > 0"
-        :disabled="this.selItems.length == 0"
-        @click="deleteMany"
-        >批量删除
-      </el-button>
-      <el-pagination
-        background
-        :page-size="pageSize"
-        layout="prev, pager, next"
-        :total="totalCount"
-        @current-change="currentChange"
-        v-show="this.articles.length > 0"
-      >
-      </el-pagination>
+
+      <div class="hot-img">
+        <img src="https://chinavolunteer.mca.gov.cn/NVSI/LEAP/Download/default/2022/4/2/96b16b2e60144cbc83e179949980eb89.png"/>
+        <h6>志愿服务关爱行动</h6> 
+      </div>
+      
     </div>
   </div>
+
+  
+
+</div>
 </template>
 
 
-
-
 <script>
-import { getRequest } from "../../utils/api";
-import { putRequest } from "../../utils/api";
-import { postRequest } from "../../utils/api";
+import {getRequest} from '../../utils/api.js'
 export default {
   name: "ArticleTable",
   data() {
     return {
-      articles: [],
+      articles: 
+      [
+        { title: "这是第一篇新闻", id: "001" ,publishDate:"2022.1.15"},
+        { title: "这是第二篇新闻", id: "002" ,publishDate:"2022.1.16"},
+        { title: "这是第三篇新闻", id: "003" ,publishDate:"2022.1.19"},
+        { title: "这是第四篇新闻", id: "004" ,publishDate:"2022.1.19"}
+      ],
+      selIems: [],
+      loading: false,
       currentPage: 1,
-      totalCount: -1,
-      pageSize: 5,
-      keywords: "",
-      type: "-1",
-      state: "1",
-      selItems: [],
-      dustbinData: [],
+      totalCount: 20,
+      pageSize: 15,
     };
   },
-
   mounted() {
-    var _this = this;
-    console.log("mouted");
-    this.loadArticles(1, this.pageSize, this.state, this.type);
-  },
-
-  methods: {
-    currentChange(currentPage) {
-      this.currentPage = currentPage;
-
-      this.loadArticles(currentPage, this.pageSize, this.state, this.type);
+        // console.log('mouted');
+        var _this = this;
+        getRequest("/article/all?state=1&page=1&count=15&type=-1").then(
+            (response) => {
+                 console.log(response.data.data);
+                _this.articles = response.data.data.records;
+            }
+        )
     },
-    loadArticles(page, count, state, type) {
-      var _this = this;
-      var url =
-        "/article/all?state=" +
-        state +
-        "&page=" +
-        page +
-        "&count=" +
-        count +
-        "&type=" +
-        type;
-      console.log(url);
-      getRequest(url).then((response) => {
-        _this.articles = response.data.data.records;
-        _this.totalCount = response.data.data.total;
-      });
-    },
-    handleSelectionChange(val) {
-      this.selItems = val;
-    },
-    handleEdit(index, row) {
+  methods:
+  {
+    handleClick(id)
+    {
       this.$router.push({
-        path: "/manage/postarticle",
-        query: { id: row.id },
-      });
-    },
-    handleDelete(index, row) {
-      console.log(index);
-      console.log(row);
-      this.dustbinData.push(row.id);
-      console.log(this.dustbinData);
-      var _this = this;
-      postRequest("/article/dustbin", {
-        aids: _this.dustbinData,
-        state: "1",
-      }).then((response) => {
-        console.log(response.data.data);
-      });
-    },
-    deleteMany() {
-      console.log("deleteMany");
-      console.log();
-      var selItems = this.selItems;
-      console.log(selItems);
-      for (var i = 0; i < selItems.length; i++) {
-        this.dustbinData.push(selItems[i].id);
-      }
-      var _this = this;
-      console.log(this.dustbinData);
-      postRequest("/article/dustbin", {
-        aids: _this.dustbinData,
-        state: "1",
-      }).then((response) => {
-        console.log(response.data.data);
-      });
-    },
-  },
+        path: "/articledetail",
+        query: {id: id}
+      })
+    }
+  }
 };
 </script>
-
-
-
-<style>
-.blog_table_footer {
-  display: flex;
-  box-sizing: content-box;
-  padding-top: 10px;
-  padding-bottom: 0px;
-  margin-bottom: 0px;
-  justify-content: space-between;
+<style scoped>
+div.article-container
+{
+  box-sizing: border-box;
+  width:50%;
+  height:auto;
+  padding:20px 10px;
+  margin:4% 1% 3% 9%;
+  background-color: rgba(245, 245, 245, 0.6);
+  border: 1px solid #f2c9bb;
+}
+ul.article-list
+{
+  list-style-type:disc;
+}
+li.article-info
+{
+  padding:6px;
+  overflow-x: hideen;
+  font-size: small;
+}
+li.article-info:hover
+{
+  background-color: rgba(255, 255, 255, 0.6);
+  color:orangered;
+  cursor: pointer;
+}
+div.hotpoint-container
+{
+  height:300px;
+  padding:10px 30px 20px 0px;
+  background-color: rgba(245, 245, 245, 0.6);
+  border: 1px solid #f2c9bb;
+  overflow: hidden;
+}
+div.row
+{
+  background-image: url(../../assets/bk.webp);
+  background-repeat:no-repeat;
+  background-size: 100%;
+  background-attachment: fixed;
+}
+div.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+h4.container-title
+{
+  margin: 20px 0px 20px 20px;
+  font-weight: bold;
+  clear: both;
+}
+div.date
+{
+  font-weight: bold;
+  opacity: 0.6;
+  overflow: hidden;
+  text-align: right;
+  height: 20px;
+}
+div.title
+{
+  height:100%;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+div.col2
+{
+  float:left;
+  box-sizing: border-box;
+  width:30%;
+  margin:4% 9% 3% 1%;
+  height:1000px;
+}
+div.hotimg-container
+{
+  height:670px;
+  padding:10px 0px 0px 0px;
+  background-color: rgba(245, 245, 245, 0.6);
+  border: 1px solid #f2c9bb;
+  margin-top:30px;
+}
+div.hot-img
+{
+  padding: 0 10% 10% 10%;
+  width: 100%;
+  height: 45%;
+  text-align: center;
+}
+div.hot-img>img
+{
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 0px;
+  margin-bottom: 2%;
 }
 </style>
