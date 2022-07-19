3921b91c7a82aa99d8856453f242a0421ac3e394
FYQ <fanyq1017@qq.com>
2022-07-19 18:21:57 +0800

    update postarticle

--- src/components/PostArticle/PostArticle.vue
+++ src/components/PostArticle/PostArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <el-container class="post-article">
-    <el-header class="header" >
+    <el-header class="header">
       <el-select
         v-model="article.type"
         placeholder="请选择"
@@ -31,8 +31,6 @@
     >
     </mavonEditor>
 
-
-    
     <el-button @click="articleSave"> 发布资讯</el-button>
   </el-container>
 </template>
@@ -41,25 +39,22 @@
 <script>
 import { mavonEditor } from "mavon-editor";
 import { postRequest } from "../../utils/api";
-import  {getRequest} from "../../utils/api"
+import { getRequest } from "../../utils/api";
 import { uploadFileRequest } from "../../utils/api";
 import "mavon-editor/dist/css/index.css";
 export default {
   name: "MarkdownEditor",
   components: { mavonEditor },
   mounted() {
-    var id = this.$route.query.id
-    var _this = this
-    if(id != null && id != '' && id != undefined){
-
-      getRequest("/article/query?id=" + id).then(
-        (response) => {
-          if(response.data.state == 200){
-            _this.article = response.data.data
-            _this.mdContent = response.data.data.mdContent
-        }}
-      )
-
+    var id = this.$route.query.id;
+    var _this = this;
+    if (id != null && id != "" && id != undefined) {
+      getRequest("/article/query?id=" + id).then((response) => {
+        if (response.data.state == 200) {
+          _this.article = response.data.data;
+          _this.mdContent = response.data.data.mdContent;
+        }
+      });
     }
   },
 
@@ -67,12 +62,16 @@ export default {
     return {
       options: [
         {
+          label: "全部",
+          value: -1,
+        },
+        {
           label: "志愿风采",
-          value: "1",
+          value: 1,
         },
         {
           label: "信息动态",
-          value: "2",
+          value: 2,
         },
       ],
       content: "",
@@ -128,8 +127,6 @@ export default {
 
       console.log("已保存");
     },
-
-
   },
 };
 </script>
